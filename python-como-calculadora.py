7534ece440c92a2ec039f2b2031811533521d8d4
Tricodando <85141942+tricodando@users.noreply.github.com>
2021-06-09 13:02:34 -0300

    Add files via upload

---
+++ python-como-calculadora.py
@@ -0,0 +1,23 @@
+'''
+    Python: 3.7.9
+    Autor: Tricodando
+    Descricao: Python como uma Calculadora '''
+
+# adicao e subtracao
+print(2+3)
+print(10-1)
+print(3-5)
+print(-2+(-3))
+
+# multiplicacao e divisao
+print(2*3)
+print(3*(-1))
+print(2*5)
+print(4/2)
+print(2/0)
+
+# potenciacao e modulo
+print(2**3)
+print(3**3)
+print(3%2)
+print(10%4)
