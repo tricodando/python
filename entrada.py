463af93156ae0b3bbbb557ffa409e93c98fbc671
Tricodando <85141942+tricodando@users.noreply.github.com>
2021-06-22 09:04:57 -0300

    Add files via upload

---
+++ entrada.py
@@ -0,0 +1,8 @@
+'''
+    Python: 3.7.9
+    Autor: Tricodando
+    Descricao: Entrada no Python '''
+
+nome = input("Digite o seu nome: ")
+cor = input("Digite uma cor: ")
+print(("Olá " + nome + " você escolheu a cor " + cor).upper())
