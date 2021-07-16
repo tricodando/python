1ef0dad7a6af59ca78dac85aa2261dc52a70cd8a
Tricodando <85141942+tricodando@users.noreply.github.com>
2021-07-16 19:23:35 -0300

    Add files via upload

---
+++ condicao.py
@@ -0,0 +1,11 @@
+'''
+    Python: 3.7.9
+    Autor: Tricodando
+    Descricao: Entrada no Python '''
+
+sexo = input("Digite o seu sexo: ")
+
+if (sexo == "m"):
+	print("Prestação de Serviço Militar Obrigatório!")
+elif (sexo == "f"):
+	print("Prestação de Serviço Militar Não Obrigatório!")
